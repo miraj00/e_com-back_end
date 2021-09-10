c94809883d7a45a4efb9de0a72691d838e732c6d
miraj <miraj00@gmail.com>
2021-09-10 17:16:03 -0400

    models added

--- models/Product.js
+++ models/Product.js
@@ -9,8 +9,44 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
-  },
+    // define columns.
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    
+    price: {
+      type: DataTypes.DECIMAL(8, 2),
+      allowNull: false,
+      Validate: {
+        isDecimal: true
+      }
+    },
+    
+    stock: {
+      type: DataTypes.INTEGER,
+       allowNull: false,
+       defaultValue: 10,
+       Validate: {
+         isNumeric: true
+       }
+    },
+   
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category', 
+        key: 'id',
+      }
+    }    
+ },
   {
     sequelize,
     timestamps: false,
