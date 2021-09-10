a83e8d5b2eda6c4f7316da40b205e8e61d819a58
miraj <miraj00@gmail.com>
2021-09-10 19:11:53 -0400

    POST route for Creating New Tag added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,8 +2,7 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-// find all tags ---------------------------------------------
-
+// find all tags ---------------------------------------------------------------
 router.get('/', (req, res) => {
    // be sure to include its associated Product data
  
@@ -23,10 +22,9 @@ router.get('/', (req, res) => {
     .then(dbTagData => res.json(dbTagData))
     .catch(err => { console.log(err);
                     res.status(500).json(err); })
-
 });
 
-// find a single tag by its `id` --------------------------
+// find a single tag by its `id` --------------------------------------------------
 router.get('/:id', (req, res) => {
   // be sure to include its associated Product data
   Tag.findOne({
@@ -52,17 +50,22 @@ router.get('/:id', (req, res) => {
                   res.status(500).json(err); })
 });
 
-
-// create a new tag --------------------------------------
+// create a new tag ---------------------------------------------------------------
 router.post('/', (req, res) => {
-});
 
+  Tag.create({
+     tag_name: req.body.tag_name
+  })
+ 
+ .then(dbTagData => res.json(dbTagData))
+ .catch(err => { console.log(err);
+                 res.status(500).json(err); });
+});
 
 
 
 // update a tag's name by its `id` value -----------------
 router.put('/:id', (req, res) => {
-});
 
 
 
