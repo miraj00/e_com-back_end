64f7d0a838d193b3ebdb1291d6fa5fcd4a108bb9
miraj <miraj00@gmail.com>
2021-09-10 18:53:05 -0400

    GET all tags and Single Tag route added

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
+
 // The `/api/products` endpoint
 // get all products----------------------------------------------
 router.get('/', (req, res) => {
